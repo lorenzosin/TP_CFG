5bd03bfe57835907a6d73048b1183eb354cefa52
lorenzosin <lorenzo.sinito96@gmail.com>
2019-01-13 19:02:10 +0100

    Parsing complete. Need to recreate the Graph

--- Parser.py
+++ Parser.py
@@ -1,6 +1,7 @@
 # Using NetworkX as library for the creation of Graphs
 import networkx as nx
-
+# Using re = regular expressions for the lines parsing
+import re
 # I am assming that the file exists and user has permissions -> No checks
 f = open("out.txt", "r")
 
@@ -12,7 +13,7 @@ while end_lecture == 0:
     row = f.readline()
     useful = row.find("<find_min>:")  # Searching starting point
     if (useful != -1):
-        code.append(row.strip())
+        # code.append(row.strip())
         while end_main == 0:
             row = f.readline()
             if row == "\n":  # Searching ending point (empity line)
@@ -24,5 +25,18 @@ while end_lecture == 0:
 # Closure of the file
 f.close()
 
-for i in code:
-    print(i)
+# Display of the code
+# for i in code:
+#     print(i)
+
+graph = nx.Graph()
+for row in code:
+    parsed = re.split("\t", row)
+    address = parsed[0]     # take first element
+    address = address[:-1]  # remove the ':' char
+    name = parsed[1]
+    instruction = parsed[2]
+    arguments = parsed[3]
+    if (instruction[0]) == "b":
+        print("branch: " + str(parsed))
+    # print(parsed)
